6676894f6fc8a2f2fdba71e7038a9654e7b45394
Cyril <manda.cyril@gmail.com>
2018-05-01 12:42:32 +0200

    Fin première partie

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,45 @@
+class GossipsController < ApplicationController
+	def new
+		@g = Gossip.new
+	end
+
+	def index
+		@list_gossips = Gossip.all
+	end
+
+	def create
+		# On utilise params.permit afin de sélectionner les arguments que l'on veut utiliser pour la création de notre gossip
+	 	g = Gossip.create(params.require(:gossip).permit(:anonymous_author, :content))
+	  	# On ajoute une redirection une fois le gossip créé
+	  	unless g == nil
+	  		redirect_to gossip_path(g.id)
+	  	# else
+	  	# 	redirect_to show_user_error_create_path
+	  	end
+	end
+
+	def show
+		# On utilise permit là encore pour des questions de sécurité : seul le paramètre en question sera recherché. 
+		# Dans ce cas, on utilse find_by car la fonction permet d'indiquer ce que l'on recherche suivi de la valeur
+		@g = Gossip.find_by(params.permit(:id))
+	end
+
+	def edit
+		# On utilise permit là encore pour des questions de sécurité : seul le paramètre en question sera recherché. 
+		# Dans ce cas, on utilse find_by car la fonction permet d'indiquer ce que l'on recherche suivi de la valeur
+		@g = Gossip.find_by(params.permit(:id))
+	end
+
+	def update
+		g = Gossip.find_by(params.permit(:id))
+	 	g.update(params.require(:gossip).permit(:anonymous_author, :content))
+	 	redirect_to gossip_path(g.id)
+	end
+
+	def destroy
+		g = Gossip.find_by(params.permit(:id))
+	 	g.destroy
+	 	redirect_to gossips_path
+	end
+
+end
